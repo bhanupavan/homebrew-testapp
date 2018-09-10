06db0556e7e9374a28c60785d38cdb853fc058f0
bhanu.bhairagimatham <bhanu.bhairagimatham@servicenow.com>
2018-09-10 17:17:32 +0530

    add formula

---
+++ testscripts.rb
@@ -0,0 +1,18 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Testscripts < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/bhanupavan/testscripts/archive/v1.0.0.tar.gz"
+  sha256 "fe7ad2e70893d580244378972a80eb753a432647b81d9d65af06a83d0d1846df"
+  # depends_on "cmake" => :build
+
+  def install
+  
+
+	bin.install "testscrpts"
+
+  end
+
+end
